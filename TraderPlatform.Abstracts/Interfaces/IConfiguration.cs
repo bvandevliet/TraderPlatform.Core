a70a8d9a775192b31117191edde937cdb78dada6
Bob Vandevliet <info@bvandevliet.nl>
2022-11-17 17:46:34 +0100

    Created class Configuration with default values.

--- TraderPlatform.Abstracts/Interfaces/IConfiguration.cs
+++ TraderPlatform.Abstracts/Interfaces/IConfiguration.cs
@@ -1,31 +1,25 @@
-using System.ComponentModel.DataAnnotations;
-
 namespace TraderPlatform.Abstracts.Interfaces;
 
-// DATA ANNOTATIONS WITHIN INTERFACE ??
 public interface IConfiguration
 {
-  IAsset QuoteCurrency { get; set; }
+  IAsset? QuoteCurrency { get; set; }
 
-  [Range(0, 100)]
   decimal QuoteAllocation { get; set; }
 
-  [Range(1, 100)]
+  Dictionary<IAsset, decimal> AltWeightingFactors { get; set; }
+
+  List<string> TagsToIgnore { get; set; }
+
   int TopRankingCount { get; set; }
 
-  [Range(1, 50)]
   decimal Smoothing { get; set; }
 
-  [Range(1, 20)]
   decimal NthRoot { get; set; }
 
-  [Range(1, double.PositiveInfinity)]
   decimal IntervalHours { get; set; }
 
-  [Range(0, double.PositiveInfinity)]
   decimal MinimumDiffQuote { get; set; }
 
-  [Range(0, 100)]
   decimal MinimumDiffAllocation { get; set; }
 
   bool AutomationEnabled { get; set; }
---
+++ TraderPlatform.Abstracts/Models/Configuration.cs
@@ -0,0 +1,39 @@
+using System.ComponentModel.DataAnnotations;
+using TraderPlatform.Abstracts.Interfaces;
+
+namespace TraderPlatform.Abstracts.Models;
+
+public class Configuration : IConfiguration
+{
+  [Required]
+  public IAsset? QuoteCurrency { get; set; } = null;
+
+  [Range(0, 100)]
+  public decimal QuoteAllocation { get; set; } = 0m;
+
+  public Dictionary<IAsset, decimal> AltWeightingFactors { get; set; } = new();
+
+  public List<string> TagsToIgnore { get; set; } = new() { "stablecoin" };
+
+  [Range(1, 100)]
+  public int TopRankingCount { get; set; } = 10;
+
+  [Range(1, 50)]
+  public decimal Smoothing { get; set; } = 4m;
+
+  [Range(1, 20)]
+  public decimal NthRoot { get; set; } = 2.5m;
+
+  [Range(1, double.PositiveInfinity)]
+  public decimal IntervalHours { get; set; } = 6m;
+
+  [Range(0, double.PositiveInfinity)]
+  public decimal MinimumDiffQuote { get; set; } = 5m;
+
+  [Range(0, 100)]
+  public decimal MinimumDiffAllocation { get; set; } = 1.2m;
+
+  public bool AutomationEnabled { get; set; } = false;
+
+  public DateTime? LastRebalance { get; set; } = null;
+}
