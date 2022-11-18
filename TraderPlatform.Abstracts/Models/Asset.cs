909a9c24053854f58c971a0080d6cdd92ae054ac
Bob Vandevliet <info@bvandevliet.nl>
2022-11-18 15:11:34 +0100

    Added doc block to Asset ctor.

--- TraderPlatform.Abstracts/Models/Asset.cs
+++ TraderPlatform.Abstracts/Models/Asset.cs
@@ -11,6 +11,11 @@ public class Asset : IAsset
   /// <inheritdoc/>
   public string Name { get; set; }
 
+  /// <summary>
+  /// <inheritdoc cref="IAsset"/>
+  /// </summary>
+  /// <param name="symbol"><inheritdoc cref="Symbol"/></param>
+  /// <param name="name"><inheritdoc cref="Name"/></param>
   public Asset(string symbol, string? name = null)
   {
     Symbol = symbol;
