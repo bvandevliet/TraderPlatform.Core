045114030eca4311194a5f140aa12d14431b986c
Bob Vandevliet <info@bvandevliet.nl>
2022-12-07 16:20:40 +0100

    A Dto should only include other Dto's or basic types.

--- TraderPlatform.Abstracts/Models/ConfigDto.cs
+++ TraderPlatform.Abstracts/Models/ConfigDto.cs
@@ -4,49 +4,49 @@ using TraderPlatform.Abstracts.Interfaces;
 namespace TraderPlatform.Abstracts.Models;
 
 /// <inheritdoc cref="IConfig"/>
-public abstract class ConfigDto : IConfig
+public abstract class ConfigDto // : IConfig
 {
-  /// <inheritdoc/>
+  /// <inheritdoc cref="IConfig.QuoteCurrency"/>
   [Required]
-  public IAsset QuoteCurrency { get; set; } = null!;
+  public AssetDto QuoteCurrency { get; set; } = null!;
 
-  /// <inheritdoc/>
+  /// <inheritdoc cref="IConfig.QuoteAllocation"/>
   [Range(0, 100)]
   public decimal QuoteAllocation { get; set; } = 0;
 
-  /// <inheritdoc/>
-  public Dictionary<IAsset, decimal> AltWeightingFactors { get; set; } = new();
+  /// <inheritdoc cref="IConfig.AltWeightingFactors"/>
+  public Dictionary<AssetDto, decimal> AltWeightingFactors { get; set; } = new();
 
-  /// <inheritdoc/>
+  /// <inheritdoc cref="IConfig.TagsToIgnore"/>
   public List<string> TagsToIgnore { get; set; } = new() { "stablecoin" };
 
-  /// <inheritdoc/>
+  /// <inheritdoc cref="IConfig.TopRankingCount"/>
   [Range(1, 100)]
   public int TopRankingCount { get; set; } = 10;
 
-  /// <inheritdoc/>
+  /// <inheritdoc cref="IConfig.Smoothing"/>
   [Range(1, 50)]
   public decimal Smoothing { get; set; } = 4;
 
-  /// <inheritdoc/>
+  /// <inheritdoc cref="IConfig.NthRoot"/>
   [Range(1, 20)]
   public decimal NthRoot { get; set; } = 2.5m;
 
-  /// <inheritdoc/>
+  /// <inheritdoc cref="IConfig.IntervalHours"/>
   [Range(1, double.PositiveInfinity)]
   public decimal IntervalHours { get; set; } = 6;
 
-  /// <inheritdoc/>
+  /// <inheritdoc cref="IConfig.MinimumDiffQuote"/>
   [Range(0, double.PositiveInfinity)]
   public decimal MinimumDiffQuote { get; set; } = 5;
 
-  /// <inheritdoc/>
+  /// <inheritdoc cref="IConfig.MinimumDiffAllocation"/>
   [Range(0, 100)]
   public decimal MinimumDiffAllocation { get; set; } = 1.2m;
 
-  /// <inheritdoc/>
+  /// <inheritdoc cref="IConfig.AutomationEnabled"/>
   public bool AutomationEnabled { get; set; } = false;
 
-  /// <inheritdoc/>
+  /// <inheritdoc cref="IConfig.LastRebalance"/>
   public DateTime? LastRebalance { get; set; } = null;
 }
