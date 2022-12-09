ff8832c17df1a22003900b83144e66ba78b5b148
Bob Vandevliet <info@bvandevliet.nl>
2022-12-09 17:42:05 +0100

    Let EF convert common collection types dynamically using reflection.

--- TraderPlatform.API/DbContexts/TraderContext.cs
+++ TraderPlatform.API/DbContexts/TraderContext.cs
@@ -1,13 +1,58 @@
 using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
+using System.Reflection;
+using System.Text.Json;
 using TraderPlatform.API.Entities;
 
 namespace TraderPlatform.API.DbContexts;
 
 public class TraderContext : DbContext
 {
-  DbSet<ConfigEntity> Configurations { get; set; } = null!;
+  public DbSet<ConfigEntity> Configurations { get; set; } = null!;
 
   public TraderContext(DbContextOptions options) : base(options)
   {
+  }
+
+  protected override void OnModelCreating(ModelBuilder modelBuilder)
+  {
+    foreach (PropertyInfo dbProp in typeof(TraderContext).GetProperties(BindingFlags.Instance | BindingFlags.Public))
+    {
+      if (!dbProp.PropertyType.Name.StartsWith("DbSet"))
+      {
+        continue;
+      }
+
+      foreach (Type entType in dbProp.PropertyType.GenericTypeArguments)
+      {
+        foreach (PropertyInfo entProp in entType.GetProperties(BindingFlags.Instance | BindingFlags.Public))
+        {
+          Type propType = entProp.PropertyType;
+
+          void build<T>()
+          {
+            modelBuilder
+              .Entity(entType)
+              .Property(entProp.Name)
+              .HasConversion(new ValueConverter<T, string>(
+                v => JsonSerializer.Serialize(v, (JsonSerializerOptions?)null),
+                v => JsonSerializer.Deserialize<T>(v, (JsonSerializerOptions?)null)!));
+          }
+
+          if (propType == typeof(ICollection<int>))
+          {
+            build<ICollection<int>>();
+          }
+          else if (propType == typeof(ICollection<string>))
+          {
+            build<ICollection<string>>();
+          }
+          else if (propType == typeof(Dictionary<string, decimal>))
+          {
+            build<Dictionary<string, decimal>>();
+          }
+        }
+      }
+    }
   }
 }
